99f09fd1980a4961f77fde49b6c3de7f3fc209ba
eliza-codes <elizabethspygt@gmail.com>
2019-07-02 09:48:10 -0700

    trying to subtract value

--- CounterCodePen/counter.js
+++ CounterCodePen/counter.js
@@ -1,5 +1,6 @@
 // here is a variable where we can store the value of the counter
 let value = 0
+let number = 1
 
 // set the header to the initial value
 document.querySelector('#value').innerHTML = value.toString();
@@ -7,16 +8,11 @@ var p1 = 1
 var p2 = 2
 
 // 1. add a handler for the `subtract` button
-document.getElementById("subtract").addEventListener("click", function(){
-myFunction(p1, p2);
-
-});
-
-function myFunction(value,b){
-var result = (value - b);
-document.getElementById("value").innerHTML = result;
+document.querySelector("subtract").onclick function(){
+    value = (value - 0);
 }
 
-// 2. add a handler for the `add` button
+// 2. add a handler for the 'add' button 
+
 
 // 3. "DRY" your code out by making a function
