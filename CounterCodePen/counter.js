fdcf0aae92320050d178f2a7ff06e90c9b22d60b
eliza-codes <elizabethspygt@gmail.com>
2019-07-02 10:56:11 -0700

    editing

--- CounterCodePen/counter.js
+++ CounterCodePen/counter.js
@@ -1,19 +1,33 @@
 // here is a variable where we can store the value of the counter
 let value = 0
-let number = 1
+//let number = 1
 
 // set the header to the initial value
 document.querySelector('#value').innerHTML = value.toString();
-var p1 = 1
+/* var p1 = 1
 var p2 = 2
 
 // 1. add a handler for the `subtract` button
-document.querySelector("subtract").onclick = function(){
-    var result = (value - 1);
-    document.getElementById("#value").innerHTML = result;
+document.querySelector("#subtract").onclick = function(){
+    value = (value - 1);
+    
+    console.log(value);
 }
 
 // 2. add a handler for the 'add' button 
-
+*/
 
 // 3. "DRY" your code out by making a function
+function change(sign){
+    if (sign == "add"){
+        value++;
+        document.querySelector("#add").innerHTML = value;
+
+    }
+
+    else{
+        value--;
+        document.querySelector("#value").innerHTML = value;
+    }
+
+}
